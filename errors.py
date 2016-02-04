b7611ddacc6e15f460411e7892d05bf6009b0eed
Nathan Malkin <nmalkin@cs.berkeley.edu>
2016-02-03 21:45:51 -0800

    Remove release tracking for Sentry
    because it didn't work on remote deploys, which lacked the git repository
    (since it is one level up)

--- errors.py
+++ errors.py
@@ -3,4 +3,4 @@ import raven
 
 sentry_url = os.getenv('SENTRY_URL')
 if sentry_url:
-    sentry = raven.Client(dsn=sentry_url, release=raven.fetch_git_sha(os.path.join(os.path.dirname(__file__), os.pardir)))
+    sentry = raven.Client(sentry_url)
