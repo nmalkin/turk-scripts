b05dc74b74cd6c129e2b2e28db01c55873b19f66
Nathan Malkin <nmalkin@cs.berkeley.edu>
2016-02-07 12:26:33 -0800

    Show warning when Sentry is not configured

--- errors.py
+++ errors.py
@@ -1,6 +1,10 @@
 import os
 import raven
 
+import logger
+
 sentry_url = os.getenv('SENTRY_URL')
 if sentry_url:
     sentry = raven.Client(sentry_url)
+else:
+    logger.warning('running without Sentry logging')
