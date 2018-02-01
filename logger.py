56ddec0f5f7a53c4c6221bf267fa6c6ca09895f7
Nathan Malkin <nmalkin@cs.berkeley.edu>
2018-02-01 12:02:05 -0800

    Allow specifying log level on initialization

--- logger.py
+++ logger.py
@@ -4,10 +4,11 @@ Set up a common logger for scripts that wish to use it
 import logging
 
 
-def init():
+def init(loglevel='info'):
     """
     Initialize preferred configuration for logging module
     """
+    level = getattr(logging, loglevel.upper())
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO)
+        level=level)
