78220a0d831c70070c768127ea041d71a99a7354
Nathan Malkin <nmalkin@cs.berkeley.edu>
2018-02-22 23:29:24 -0800

    Enforce Python version

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name='mturk',
       author='nmalkin',
       license='BSD',
       packages=['mturk'],
+      python_requires='>=3.6',
       install_requires=[
           'boto3>=1.5,<2',
       ],
