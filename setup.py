1b17d82389e00bd7af37ebc3b3a7a752f353fbbf
Nathan Malkin <nmalkin@cs.berkeley.edu>
2018-03-16 19:10:48 -0700

    Allow notifying workers about granted qualifications
    through flag to command

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mturk',
-      version='0.1.1',
+      version='0.1.2',
       description="Utilities to help interact with Amazon's Mechanical Turk API",
       url='http://github.com/nmalkin/mturk',
       author='nmalkin',
